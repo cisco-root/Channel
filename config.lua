1a31f8cb3c7c2cfb40cc9e411b02d19ed4294a99
Cisco <40459710+cisco-root@users.noreply.github.com>
2019-05-10 13:31:00 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "816851952:AAHY0NkiORApKtODQQ2eJubew2hbLmKDfNM"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 534937278
